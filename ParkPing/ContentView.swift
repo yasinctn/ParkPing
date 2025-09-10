66a5f0ec0149951db38053d52c557cec9319c91c
Yasin Cetin <yasincetin2014@gmail.com>
2025-09-10 13:18:58 +0300

    Initial Commit

---
+++ ParkPing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ParkPing
+//
+//  Created by Yasin Cetin on 10.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ParkPing/ParkPingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkPingApp.swift
+//  ParkPing
+//
+//  Created by Yasin Cetin on 10.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct ParkPingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
